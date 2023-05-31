3784276c06456a0141c8f06f5f990a33ecf3cde8
isaaclhy13 <hlee777@wisc.edu>
2023-05-31 13:08:31 -0400

    phone num

--- src/TenantCard/tenantCard.js
+++ src/TenantCard/tenantCard.js
@@ -1,12 +1,21 @@
 import { useEffect, useState } from "react";
-import { Button } from "@mui/material";
+import { Modal, TextField} from "@mui/material";
+import Button from '@mui/material/Button';
+
 
 export default function TennatCard(props) {
     const tenant = props.data
-    console.log(props)
     const [prop, setProp] = useState(null)
     const [phoneNum, setPhoneNum] = useState(props.data.phoneNumber)
     const [showPhoneNum, setShowPhoneNum] = useState(false)
+    const [userPN, setUserPN] = useState(null)
+    const [userName, setUserName] = useState(null)
+
+
+    //Phone Number Modal
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
     
     useState(()=>{
         fetchPropData()
@@ -26,6 +35,61 @@ export default function TennatCard(props) {
         return Math.floor(ms / (1000*60*60*24*365));
     }
 
+    async function handlePhoneNumberPress(){
+        let pn = localStorage.getItem("phoneNumber")
+        let name = localStorage.getItem("name")
+
+        if(pn == null){
+            setOpen(true)
+        }
+        else if(name == null){
+            setOpen(true)
+        }
+        else{
+            addToContactList(name, pn)
+            setShowPhoneNum(true)
+        }
+
+
+        //Handle Press
+    }
+
+    async function addToContactList(name, phoneNum) {
+     
+        await fetch('https://crib-llc.herokuapp.com/users/addContactedBy', {
+            method: 'POST',
+            headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+        },
+            body: JSON.stringify({
+                "name": name,
+                "phoneNumber": phoneNum,
+                "postedById": tenant._id
+            })
+        }).catch(e => {
+            alert(e)
+        })
+    }
+
+    function handlePhoneNumberSubmit(){
+        if(userName.trim() == ""){
+            alert("Name cannot be empty")
+            return;
+        }
+        if(userPN.trim() == ""){
+            alert("Phone Number cannot be empty")
+            return;
+        }
+
+        localStorage.setItem("phoneNumber", userPN)
+        localStorage.setItem("name", userName)
+        
+        addToContactList(userName, userPN)
+        //call api to document contacted by 
+        setOpen(false)
+        setShowPhoneNum(true)
+    }
     
 
     
@@ -73,14 +137,43 @@ export default function TennatCard(props) {
 
            
             <div style={{flexDirection:'row', justifyContent:'space-between', alignItems:'center', display:'flex', marginTop: 20, marginBottom: 10}}>
-                <Button onClick={()=> setShowPhoneNum(true)} style={{backgroundColor:'#2D6674'}} variant="contained">
+                <Button onClick={handlePhoneNumberPress} style={{backgroundColor:'#2D6674'}} variant="contained">
                     Show Phone Number
                 </Button>
                 <h5 style={{fontWeight: '600'}}>${prop.price} <span style={{fontWeight:'500', fontSize:15, color: '#333333'}}> /month</span></h5>
             </div>
 
+            <Modal 
+            
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+            style={{
+                alignItems: 'center',
+                justifyContent: 'center',
+                flex: 1,
+                display: 'flex',
+                position: 'relative'
+            }}
+            >
+                <div style={{width: '90vw', height: '50vh', backgroundColor:'white', alignSelf:'center', maxWidth: 400, borderRadius:20, padding: 20}}>
+                    <h4 style={{fontWeight:'700'}}>Enter your phone number</h4>
+                    <p>We will keep you updated when new subleases around the area is posted.</p>
+                    <div style={{marginTop: 20}}>
+                        <TextField onChange={(val)=>setUserName(val.target.value)} fullWidth label="Name" variant="outlined"/>
+                        <TextField onChange={(val)=>setUserPN(val.target.value)} type="number" fullWidth label="Phone number" variant="outlined" style={{marginTop: 15}}/>
+                        <small>* Enter a US phone number if possible<br/>* Only enter digits, no dashes(-) or spaces( )</small>
+                    </div>
+                    
+                    <Button onClick={handlePhoneNumberSubmit} style={{position:'absolute', bottom: 20, backgroundColor: '#2D6674', }} variant="contained">Submit</Button>
+                    
+                </div>
+            </Modal>
+            
         </>
         }
+        
         </div>
     )
 }
